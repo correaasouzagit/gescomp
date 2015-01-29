ca0d0a77701f6a114137d2d36486f692565a5373
Alexandre <correaasouza@gmail.com>
2015-01-29 20:50:48 -0200

    Autenticação com devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
+  resources :events
+
   resources :pessoa_juridicas
 
   # The priority is based upon order of creation: first created -> highest priority.
